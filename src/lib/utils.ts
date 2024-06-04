f264e82f07ce9501dbecc1fa7baf82be8899f7d6
yogyy <yogimr261@gmail.com>
2024-06-04 22:05:41 +0700

    chore: add chat date helper

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,10 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export function toDate(date: number) {
+  const msgDate = new Date(date * 1000);
+  const hours = msgDate.getHours();
+  const minutes = msgDate.getMinutes();
+  return `${hours.toString().padStart(2, "0")}:${minutes.toString().padStart(2, "0")}`;
+}
