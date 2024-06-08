7a735fb52321d1367aaadbd9d103b8ef7365dd01
yogyy <yogimr261@gmail.com>
2024-06-08 17:48:52 +0700

    feat: add sonner

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
+import { Toaster } from "sonner";
 import ChatApp from "./components/chat-app";
 
 function App() {
-  return <ChatApp />;
+  return (
+    <>
+      <ChatApp />
+      <Toaster
+        position="top-right"
+        toastOptions={{ className: "bg-[#0b121590] text-white" }}
+      />
+    </>
+  );
 }
 
 export default App;
