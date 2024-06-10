44528b58ea79d771440811786fd32a755b3f5861
yogyy <yogimr261@gmail.com>
2024-06-10 23:37:39 +0700

    chore: add double checkmark icon

--- src/components/icons/index.tsx
+++ src/components/icons/index.tsx
@@ -255,3 +255,23 @@ export const PencilSquare = ({ className, ...props }: IconProps) => {
     </svg>
   );
 };
+
+export const DoubleCheck = ({ ...props }: IconProps) => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      {...props}
+    >
+      <path d="M18 6 7 17l-5-5" />
+      <path d="m22 10-7.5 7.5L13 16" />
+    </svg>
+  );
+};
